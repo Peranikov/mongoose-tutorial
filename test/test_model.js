619b16fda5416db8f268e7543970bef61eac8e4e
Peranikov <ym.nekoze@gmail.com>
2013-10-11 00:11:54 +0900

    add .travis.yml

--- test/test_model.js
+++ test/test_model.js
@@ -5,22 +5,36 @@ require('should');
 var _Post = require("../model").Post;
 
 describe('#findByName', function() {
-    it('指定したnameのデータが取得できるべき', function(done) {
-      removeAndSave(function(err) {
-        if (err) {
-          console.log(err);
-        }
 
-        _Post.findByName("Jack", function(err, docs) {
-          if (err) {
-            console.log(err);
-          }
-
-          docs[0].name.should.equal("Jack");
-          done();
-        });
-      });
+  before(function(done) {
+    removeAndSave(function(err) {
+      if (err) {
+        console.log(err);
+      }
+      done();
     });
+  });
+
+  it('指定したnameのデータが取得できるべき', function(done) {
+    _Post.findByName("Jack", function(err, docs) {
+      if (err) {
+        console.log(err);
+      }
+
+      docs[0].name.should.equal("Jack");
+      done();
+    });
+  });
+
+
+  after(function(done) {
+    _Post.remove({}, function(err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+    done();
+  });
 });
 
 function save(cb) {
